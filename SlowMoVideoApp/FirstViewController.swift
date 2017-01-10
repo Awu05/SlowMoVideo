a32216c0617017c48e3ff1cd263c98ddc0fd771a
Andy Wu <andywu@Andys-Mac.local>
2017-01-10 15:30:34 -0500

    Initial Commit

---
+++ SlowMoVideoApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SlowMoVideoApp
+//
+//  Created by Andy Wu on 1/10/17.
+//  Copyright © 2017 Andy Wu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
